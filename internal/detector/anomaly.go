45f15062f961d45b75057b7f3308ee629c93e671
ellisdeibroncci <alexander.delbrocco@proton.me>
2026-04-09 15:15:48 +0100

    feat: rudemenry portscan and brute force monitoring

--- internal/detector/anomaly.go
+++ internal/detector/anomaly.go
@@ -9,6 +9,9 @@ import (
 
 const (
 	failedLoginThreshold = 5
+	portScanThreshold		 = 10
+	trafficThreshold     = 20
+
 	windowSeconds        = 60
 )
 
@@ -26,18 +29,57 @@ type Detector struct {
 // ----------------------------------------------------------------------------
 
 func (d *Detector) detect() []Anomaly {
-    allEvents := d.store.ListAll()
-    seen := map[string]bool{}
-    var anomalies []Anomaly
-    for _, event := range allEvents {
-			ip := event.SourceIP
-			if seen[ip] { continue }
-			seen[ip] = true
-			if d.recentBadLogins(ip, allEvents) > failedLoginThreshold {
-				anomalies = append(anomalies, Anomaly{Source: ip})
-			}
-    }
-    return anomalies
+// Detect anomalies based on events in the store
+
+	allEvents := d.store.ListAll()
+	seen := map[string]bool{}
+	var anomalies []Anomaly
+
+	// Iterate through all events, considering unique IPs for anomaly
+	for _, event := range allEvents {
+
+		// Skip if IP has been processed before
+		ip := event.SourceIP
+		if seen[ip] { continue }
+
+		// For each unique IP, check for recent failed logins
+		seen[ip] = true
+		if d.recentBadLogins(ip, allEvents) > failedLoginThreshold {
+			anomalies = append(
+				anomalies, Anomaly{
+					Source: ip,
+					Events: matchingEvents(ip, allEvents),
+					Type:   events.FailedLogin,
+				},
+			)
+		}
+
+		// For each IP, check for multiple port scanning events in time window
+		if d.recentPortScans(ip, allEvents) > portScanThreshold {
+			anomalies = append(
+				anomalies,
+				Anomaly {
+					Source: ip,
+					Events: matchingEvents(ip, allEvents),
+					Type: events.PortScan,
+				},
+			)
+		}
+
+		// For each IP check for suspicious amounts of traffic from ip
+		if d.highEventVolume(ip, allEvents) > trafficThreshold {
+			anomalies = append(
+				anomalies, 
+				Anomaly {
+					Source: ip,
+					Events: matchingEvents(ip, allEvents),
+					Type: events.AnomalousTraffic,
+				},
+			)
+		}
+	}
+
+	return anomalies
 }
 
 
@@ -62,6 +104,47 @@ func (d *Detector) recentBadLogins(ip string, allEvents []events.Event) int {
 }
 
 
+func (d *Detector) recentPortScans(ip string, allEvents []events.Event) int {
+	// Count port scans
+	
+	seen := map[int]bool{}
+
+	for _, event := range allEvents {
+
+		// Sift to IP that we haven't seen
+		if event.SourceIP != ip          { continue }
+		if event.Type != events.PortScan { continue }
+
+		// Count recent port scans
+		if time.Since(event.Timestamp) < time.Duration(windowSeconds)*time.Second {
+			seen[event.Port] = true
+		}
+	}
+
+	return len(seen)
+}
+
+
+func (d *Detector) highEventVolume(ip string, allEvents []events.Event) int {
+	// Detects high number of volumes processed for a user
+
+	count := 0
+
+	// Iterate throughout all events
+	for _, event := range allEvents {
+
+		// Sift to ip we haven't seen
+		if event.SourceIP != ip { continue }
+
+		//Count events within time window
+		if time.Since(event.Timestamp) < time.Duration(windowSeconds)*time.Second {
+			count++
+		}
+	}
+
+	return count
+}
+
 // ----------------------------------------------------------------------------
 
 func New(store *events.Store) *Detector {
@@ -76,4 +159,19 @@ func (d *Detector) Summary(w http.ResponseWriter, r * http.Request) {
 	json.NewEncoder(w).Encode(d.detect())
 
 	return
+}
+
+func matchingEvents(ip string, allEvents []events.Event) []events.Event {
+	// Return all events matching the given IP
+
+	var matches []events.Event
+
+	// Iterate through all events, matching those by IP.
+	for _, event := range allEvents {
+		if event.SourceIP == ip {
+			matches = append(matches, event)
+		}
+	}
+
+	return matches
 }
